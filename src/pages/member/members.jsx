d888856ebdc511cf512777487258ed12bd5c2251
h1Vincent/h1 <vincentgasatan21@gmail.com>
2026-08-31 17:24:12 +0800

    Updated all pages

--- src/pages/member/members.jsx
+++ src/pages/member/members.jsx
@@ -10,17 +10,17 @@ function Members() {
               <img src={members.chibiimage} alt="members chibi" />
             </section>
              <section className="hovercard">
-              <aside className="hoveremoji">
-                  <h2> {members.stagename} </h2>
-`                 <h3> {members.emoji} </h3>
-              </aside>
-              <img className="hoverimage" src={members.image} alt="members images" />
-              <aside className="hoverinfo">
-              <h2>  {members.name}</h2>
-              <h2> {members.age}</h2>
-              <h2> {members.role} </h2>
-              <h2> {members.nationality} </h2>
-              </aside>
+                <aside className="hoveremoji">
+                    <h2> {members.stagename} </h2>
+  `                 <h3> {members.emoji} </h3>
+                </aside>
+                <img className="hoverimage" src={members.image} alt="members images" />
+                <aside className="hoverinfo">
+                <h2>  {members.name}</h2>
+                <h2> {members.age}</h2>
+                <h2> {members.role} </h2>
+                <h2> {members.nationality} </h2>
+                </aside>
             </section>  
         </section>
       ))}
